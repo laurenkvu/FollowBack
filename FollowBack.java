46ab7df219dab9520dbdeaae8e1157628340b782
Lauren Vu <lkvu175@gmail.com>
2025-01-04 21:44:26 -0600

    Update FollowBack.java
    added javadocs and FileNotFoundException handling

--- FollowBack.java
+++ FollowBack.java
@@ -1,36 +1,42 @@
-import java.io.*;
+/** 
+ * The FollowBack class provides a means of providing the user with a file of Instagram users who do not follow the account back (given a following and following text file containing respective users).
+ * @author Lauren Vu
+ */
 import java.util.*;
 
 public class FollowBack {
     public static void main (String[] args) throws FileNotFoundException {
-        System.out.println("instagram followback checker! for those who like to keep their following:follower ratio in check... ");
-        System.out.println("NOTE! to use this, you must have pre-existing text files for both following and follower usernames.");
-        PrintWriter out = new PrintWriter(new File("out.txt"));
-        Scanner in = new Scanner(System.in);
-        System.out.print("please input following file name: ");
-        String follows = in.next();
-        System.out.print("please input follower file name: ");
-        String follower = in.next();
-        in.close();
+        try {
+            System.out.println("instagram followback checker! for those who like to keep their following:follower ratio in check... ");
+            System.out.println("NOTE! to use this, you must have pre-existing text files for both following and follower usernames.");
+            PrintWriter out = new PrintWriter(new File("out.txt"));
+            Scanner in = new Scanner(System.in);
 
-        TreeSet<String> following = new TreeSet<String>();
-        Scanner scan = new Scanner(new File(follows));
-        while (scan.hasNextLine()) following.add(scan.nextLine());
+            System.out.print("please input following file name: ");
+            String follows = in.next();
+            TreeSet<String> following = new TreeSet<String>();
+            Scanner scan = new Scanner(new File(follows));
+            while (scan.hasNextLine()) following.add(scan.nextLine());
 
-        TreeSet<String> followers = new TreeSet<String>();
-        scan = new Scanner(new File(follower));
-        while (scan.hasNextLine()) followers.add(scan.nextLine());
-        scan.close();
+            System.out.print("please input follower file name: ");
+            String follower = in.next();
+            in.close();
+            TreeSet<String> followers = new TreeSet<String>();
+            scan = new Scanner(new File(follower));
+            while (scan.hasNextLine()) followers.add(scan.nextLine());
+            scan.close();
 
-        following.removeAll(followers);
-        System.out.print("wow! " + following.size() + " accounts don't follow you back! ");
+            following.removeAll(followers);
+            System.out.print("wow! " + following.size() + " accounts don't follow you back! ");
 
-        Iterator<String> it = following.iterator();
-        
+            Iterator<String> it = following.iterator();
+            
+            do out.println(it.next()); while (it.hasNext());
+            out.close();
 
-        do out.println(it.next()); while (it.hasNext());
-        out.close();
-
-        System.out.println("accounts printed to out.txt.");
+            System.out.println("accounts printed to out.txt.");
+        } catch (FileNotFoundException e) {
+            System.out.println("oops! file not found... run program again.");
+        }
     }
 }
