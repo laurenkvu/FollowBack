4ffd0479e33455b5105d441550f1996e905ecb16
Lauren Vu <lkvu175@gmail.com>
2025-01-30 10:56:21 -0600

    Update FollowBack.java
    made compatible with FollowBackViewer

--- FollowBack.java
+++ FollowBack.java
@@ -1,42 +1,44 @@
-/** 
- * The FollowBack class provides a means of providing the user with a file of Instagram users who do not follow the account back (given a following and following text file containing respective users).
- * @author Lauren Vu
- */
+import java.io.*;
 import java.util.*;
 
 public class FollowBack {
-    public static void main (String[] args) throws FileNotFoundException {
-        try {
-            System.out.println("instagram followback checker! for those who like to keep their following:follower ratio in check... ");
-            System.out.println("NOTE! to use this, you must have pre-existing text files for both following and follower usernames.");
-            PrintWriter out = new PrintWriter(new File("out.txt"));
-            Scanner in = new Scanner(System.in);
+    PrintWriter out;
+    TreeSet<String> following = new TreeSet<String>();
+    TreeSet<String> followers = new TreeSet<String>();
 
-            System.out.print("please input following file name: ");
-            String follows = in.next();
-            TreeSet<String> following = new TreeSet<String>();
-            Scanner scan = new Scanner(new File(follows));
-            while (scan.hasNextLine()) following.add(scan.nextLine());
 
-            System.out.print("please input follower file name: ");
-            String follower = in.next();
-            in.close();
-            TreeSet<String> followers = new TreeSet<String>();
-            scan = new Scanner(new File(follower));
-            while (scan.hasNextLine()) followers.add(scan.nextLine());
-            scan.close();
+    public FollowBack(String follows, String follower) throws FileNotFoundException { 
+        out = new PrintWriter(new File("out.txt"));
+        
+        Scanner scan = new Scanner(followSpitter(follows));
+        while (scan.hasNextLine()) following.add(scan.nextLine());
 
-            following.removeAll(followers);
-            System.out.print("wow! " + following.size() + " accounts don't follow you back! ");
+        
+        scan = new Scanner(followSpitter(follower));
+        while (scan.hasNextLine()) followers.add(scan.nextLine());
+        scan.close();
 
-            Iterator<String> it = following.iterator();
-            
-            do out.println(it.next()); while (it.hasNext());
-            out.close();
+        following.removeAll(followers);
+    }
 
-            System.out.println("accounts printed to out.txt.");
-        } catch (FileNotFoundException e) {
-            System.out.println("oops! file not found... run program again.");
+    public void toFile() {
+        Iterator<String> it = following.iterator();
+        do out.println(it.next()); while (it.hasNext());
+        out.close();
+    }
+
+    public int getGhosters() {
+        return following.size();
+    }
+
+    public static String followSpitter(String file) throws FileNotFoundException {
+        File fi = new File(file);
+        Scanner scam = new Scanner(fi);
+        String out = "";
+        while (scam.hasNext()) {
+            String bean = scam.nextLine().trim();
+            if (bean.indexOf("value") != -1) out += bean.substring(10, bean.length()-2)+"\n";
         }
+        return out;
     }
 }
